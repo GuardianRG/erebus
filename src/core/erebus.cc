d748694f5995e55faee9d9a822b3d222e9f9c10e
Johannes Werner <johs.werner@googlemail.com>
2015-06-27 17:57:45 +0200

    Implements simple window

--- src/core/erebus.cc
+++ src/core/erebus.cc
@@ -1,5 +1,13 @@
-#include <iostream>
+#include <gtkmm.h>
 
-int main() {
-	std::cout << "Hello world";
+int main(int argc, char *argv[])
+{
+	Glib::RefPtr<Gtk::Application> app =
+	Gtk::Application::create(argc, argv,
+				 "org.gtkmm.examples.base");
+	
+	Gtk::Window window;
+	window.set_default_size(200, 200);
+	
+	return app->run(window);
 }
