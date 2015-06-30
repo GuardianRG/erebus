3d656c50a718fa135a46d5b650baeaf4fccf5c7e
Johannes Werner <johs.werner@googlemail.com>
2015-06-30 18:21:07 +0200

    Add filereader

---
+++ include/file_reader.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+#include "types.h"
+
+namespace erebus {
+
+class FileReader {
+public:
+	static binVec readFile(std::string path);
+};
+}
---
+++ include/types.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <stdint.h>
+
+typedef uint8_t	byte;
+typedef int8_t	sbyte;
+
+typedef std::vector<byte> binVec;
