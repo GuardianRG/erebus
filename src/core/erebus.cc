9aded4747c9c66e086e2b84f1bf5e1c96b5c827b
Johannes Werner <johs.werner@googlemail.com>
2015-07-02 22:32:30 +0200

    Load UI from file

--- src/core/erebus.cc
+++ src/core/erebus.cc
@@ -3,13 +3,34 @@
 #include <main_view_presenter.h>
 
 int main(int argc, char *argv[]) {
-	Glib::RefPtr<Gtk::Application> app =
-	Gtk::Application::create(argc, argv,
-				 "org.suppengeist.erebus");
+	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 	
-	erebus::MainView mf;
-	erebus::MainViewPresenter mvp(&mf);
-	app->run(mf);
+	Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+	try
+	{
+		refBuilder->add_from_file("view1.glade");
+	}
+	catch(const Glib::FileError& ex)
+	{
+		std::cerr << "FileError: " << ex.what() << std::endl;
+		return 1;
+	}
+	catch(const Glib::MarkupError& ex)
+	{
+		std::cerr << "MarkupError: " << ex.what() << std::endl;
+		return 1;
+	}
+	catch(const Gtk::BuilderError& ex)
+	{
+		std::cerr << "BuilderError: " << ex.what() << std::endl;
+		return 1;
+	}
+	
+	Gtk::Window* window=nullptr;
+	refBuilder->get_widget("window1", window);
+	if(window) {
+		app->run(*window);
+	}
 	
 	return 0;
 }
